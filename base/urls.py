7c501590f623b1906a2cf31af17c2f5e4e332fe2
vrn295 <varun.org295@gmail.com>
2020-09-19 17:37:19 +0530

    Add Delete and Update

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('update_task/<str:pk>/', views.update, name='update_task'),
+    path('delete/<str:pk>/', views.delete, name='delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -20,3 +20,30 @@ def index(request):
         'form': form
     }
     return render(request, 'base/home.html', context)
+
+
+def update(request, pk):
+    task = Base.objects.get(id=pk)
+    form = BaseForm(instance=task)
+
+    if request.method == "POST":
+        form = BaseForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'base/update.html', context)
+
+
+def delete(request, pk):
+    item = Base.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'base/delete.html', context)
